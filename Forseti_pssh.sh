c32df9b9a448e1c77c64dbf1115f9176f2554b30
Th3Fall3nAng3l <98032018+Th3Fall3nAng3l@users.noreply.github.com>
2022-06-10 11:00:33 +0200

    Add files via upload

--- Forseti_pssh.sh
+++ Forseti_pssh.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-#mkdir ./Temp_Expinfo_CyLR
-#cd ./Temp_Expinfo_CyLR
-#mkdir ./Forseti_pssh
-#cd ./Forseti_pssh
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+#                                                                         *
+# Ce programme secondaire sera téléchargé par les autres hôtes depuis un  *
+# répertoire Github, sera lancé puis supprimé sans aucune intéraction.    *
+#                                                                         *
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
 
 NOW=$( date '+%F' )
 
@@ -28,9 +30,10 @@ disown
 wget -q https://github.com/orlikoski/CyLR/releases/download/2.2.0/CyLR_linux-x64.zip
 unzip CyLR_linux-x64.zip > /dev/null
 rm -rf CyLR_linux-x64.zip
-./CyLR -of "$NOW"_"$HOSTNAME"_CyLR.zip > /dev/null
+./CyLR -q -of "$NOW"_"$HOSTNAME"_CyLR.zip >/dev/null
+curl -s --ftp-ssl --insecure -T "$NOW"_"$HOSTNAME"_CyLR.zip ftp://$Server:$ServerPort/$ServerRep/ --user $User:$Passwd
 cd ./../../
-#rm -rf ./Temp_Expinfo_CyLR
+rm -rf ./Temp_Expinfo_CyLR
 
 printf '[\342\234\224] Récupération terminée.\n' | iconv -f UTF-8
 
