833d608a9b1c721dbf4fff69ce5e433679e6385d
Th3Fall3nAng3l <98032018+Th3Fall3nAng3l@users.noreply.github.com>
2022-06-28 12:08:29 +0200

    Add files via upload

--- Forseti_pssh.sh
+++ Forseti_pssh.sh
@@ -17,30 +17,10 @@ ServerPort="22731"
 
 ServerRep="FTP_EXPINFO/Forseti_DFIR/"
 
-echo -e "\nRécupération des données importantes, cette opération peut être longue..."
-
-spin() {
-  spinner='/|\\-/|\\-'
-  while :
-  do
-    for i  in `seq 0 7`
-    do 
-      echo -n "${spinner:$i:1}"
-      echo -en "\010"
-      sleep 0.2
-    done
-  done
-}
-spin &
-SPIN_ID=$!
-disown
-
 curl -s --ftp-ssl --insecure ftp://$Server:$ServerPort/FTP_EXPINFO/CyLR/CyLRLinux.zip -u $User:$Passwd --output CyLRLinux.zip
 unzip CyLRLinux.zip > /dev/null
 rm -rf CyLRLinux.zip
 ./CyLR -q -of "$HOSTNAME"_CyLR.zip >/dev/null
 curl -s --ftp-ssl --insecure -T "$HOSTNAME"_CyLR.zip ftp://$Server:$ServerPort/$ServerRep/ --user $User:$Passwd
 cd ./../../
-rm -rf ./Temp_Expinfo_CyLR
-
-kill -9 $SPIN_ID > /dev/null
+rm -rf ./Temp_Expinfo
